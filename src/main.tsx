6ad9031eb3aa17d56d124e5ddc47a092d66471e0
mramansingh1991-lang <mramansingh1991@gmail.com>
2026-06-06 02:15:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+// Hide the splash screen once React is mounted
+SplashScreen.hide().catch(console.error);
